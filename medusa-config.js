28649e489635202defce700b42825d0b7781be20
Usman209 <usmanjamil196@gmail.com>
2024-10-02 17:04:53 +0500

    changes in url

--- medusa-config.js
+++ medusa-config.js
@@ -1,20 +1,3 @@
-const { loadEnv, defineConfig } = require('@medusajs/framework/utils')
-
-loadEnv(process.env.NODE_ENV, process.cwd())
-
-module.exports = defineConfig({
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
-
 
 const { loadEnv, defineConfig, Modules } = require('@medusajs/framework/utils')
 
@@ -41,7 +24,7 @@ module.exports = defineConfig({
             id: "local",
             options: {
               upload_dir: "uploads",
-              backend_url: 'http://203.161.43.125:9000'
+              backend_url: 'https://grocer.al-mizan.store'
             },
           },
         ],
